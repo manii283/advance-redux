781f383d7510593fc83d9599672602f4f0505a51
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-07-18 21:30:24 +0530

    added cart quantity

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,24 +1,18 @@
-import ProductItem from "./ProductItem";
-import classes from "./Products.module.css";
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
 
 const DUMMY_PRODUCTS = [
   {
-    id: "1",
-    price: "6",
-    title: "My First Book",
-    description: "This is a first product - amazing!",
+    id: 'p1',
+    price: 6,
+    title: 'My First Book',
+    description: 'The first book I ever wrote',
   },
   {
-    id: "2",
-    price: "6",
-    title: "My Second Book",
-    description: "This is a first product - amazing!",
-  },
-  {
-    id: "3",
-    price: "6",
-    title: "My Third Book",
-    description: "This is a first product - amazing!",
+    id: 'p2',
+    price: 5,
+    title: 'My Second Book',
+    description: 'The second book I ever wrote',
   },
 ];
 
@@ -27,14 +21,15 @@ const Products = (props) => {
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        {DUMMY_PRODUCTS.map((product) => {
+        {DUMMY_PRODUCTS.map((product) => (
           <ProductItem
             key={product.id}
+            id={product.id}
             title={product.title}
             price={product.price}
             description={product.description}
-          />;
-        })}
+          />
+        ))}
       </ul>
     </section>
   );
